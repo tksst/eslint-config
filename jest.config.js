0564cfa5d88f9231e6aa46712b71cef1f39d36e5
Takashi Sato <takashi@tks.st>
2022-04-16 10:00:55 +0000

    feat: modify from tksst/typescript-node-starter

--- jest.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('@jest/types/build/Config').InitialOptions} */
-module.exports = {
-    preset: "ts-jest",
-    testMatch: ["<rootDir>/src/**/*.test.ts"],
-    collectCoverage: true,
-    errorOnDeprecated: true,
-    resetMocks: true,
-};
