6cf661380e39985067e5e6fe286ff0489de38828
Takashi Sato <takashi@tks.st>
2022-04-16 16:16:29 +0900

    Initial commit (from https://github.com/tksst/typescript-node-starter/tree/18add1237ebc27e8245ffaae9c29dc68e7fe75ab)

---
+++ prettier.config.js
@@ -0,0 +1,5 @@
+/** @type {import('prettier').Options} */
+module.exports = {
+    // eslint-disable-next-line import/no-extraneous-dependencies, global-require
+    ...require("@tksst/prettier-config"),
+};
