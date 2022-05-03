09863b123ef8becd455c26fe3a5bffdb496a1a4f
Takashi Sato <takashi@tks.st>
2022-05-03 16:36:39 +0900

    test: add snapshot test for ESLint config

---
+++ config-snapshot.test.js
@@ -0,0 +1,29 @@
+const { ESLint } = require("eslint");
+const path = require("path");
+
+// ESLint's parser settings cannot be shared as is because it is absolute path.
+function relativize(config) {
+    if (config.parser !== undefined && config.parser !== null) {
+        config.parser = path.relative(".", config.parser);
+    }
+}
+
+describe("ESLint config Snapshot", () => {
+    let eslint;
+
+    beforeAll(() => {
+        eslint = new ESLint({ extensions: [".js", ".ts"] });
+    });
+
+    it("for JavaScript", async () => {
+        const result = await eslint.calculateConfigForFile("./test.js");
+        relativize(result);
+        expect(result).toMatchSnapshot();
+    });
+
+    it("for TypeScript", async () => {
+        const result = await eslint.calculateConfigForFile("./test.ts");
+        relativize(result);
+        expect(result).toMatchSnapshot();
+    });
+});
