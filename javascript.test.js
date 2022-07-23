03cf4df571f85a9fa7827edde5403798924248d8
Takashi Sato <takashi@tks.st>
2022-07-24 01:01:22 +0900

    feat: Local imports now always require the extension
    This is in line with the recent trend in ES Module.

--- javascript.test.js
+++ javascript.test.js
@@ -1,5 +1,5 @@
 const { ESLint } = require("eslint");
-const relativize = require("./relativize");
+const relativize = require("./relativize.js");
 
 describe("ESLint config Snapshot", () => {
     let eslint;
--- typescript.js
+++ typescript.js
@@ -18,16 +18,6 @@ module.exports = {
         "@typescript-eslint/no-unused-vars": "warn",
         "prefer-const": "warn",
         // from https://github.com/iamturns/eslint-config-airbnb-typescript/blob/4aec5702be5b4e74e0e2f40bc78b4bc961681de1/lib/shared.js#L230-L240
-        "import/extensions": [
-            "error",
-            "ignorePackages",
-            {
-                js: "never",
-                mjs: "never",
-                jsx: "never",
-                ts: "never",
-                tsx: "never",
-            },
-        ],
+        "import/extensions": ["error", "ignorePackages"],
     },
 };
