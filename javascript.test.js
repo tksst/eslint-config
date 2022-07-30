810d5eb58412756981b5fa4a01506f982a7694b9
Takashi Sato <takashi@tks.st>
2022-07-30 23:21:42 +0900

    test: refactor tests

--- javascript.test.js
+++ javascript.test.js
@@ -1,30 +1,29 @@
 const { ESLint } = require("eslint");
 const relativize = require("./relativize.js");
 
+const eslint = new ESLint({
+    useEslintrc: false,
+    overrideConfigFile: "./javascript.js",
+    extensions: [".js", ".cjs", ".mjs"],
+});
+
 describe("ESLint config Snapshot", () => {
-    let eslint;
-
-    beforeAll(() => {
-        eslint = new ESLint({
-            useEslintrc: false,
-            overrideConfigFile: "./javascript.js",
-            extensions: [".js", ".cjs", ".mjs"],
-        });
-    });
-
     it("for JavaScript", async () => {
         const result = await eslint.calculateConfigForFile("./test.js");
         relativize(result);
         expect(result).toMatchSnapshot();
     });
-    it("for JavaScript cjs", async () => {
-        const result = await eslint.calculateConfigForFile("./test.cjs");
-        relativize(result);
-        expect(result).toMatchSnapshot();
+});
+
+describe("config between extensions", () => {
+    let jsP;
+    beforeAll(() => {
+        jsP = eslint.calculateConfigForFile("./test.js");
     });
-    it("for JavaScript mjs", async () => {
-        const result = await eslint.calculateConfigForFile("./test.mjs");
-        relativize(result);
-        expect(result).toMatchSnapshot();
+    it(".cts", async () => {
+        expect(await jsP).toStrictEqual(await eslint.calculateConfigForFile("./test.cjs"));
+    });
+    it(".mts", async () => {
+        expect(await jsP).toStrictEqual(await eslint.calculateConfigForFile("./test.mjs"));
     });
 });
