40449dbcbefae041a6320e500ac9f09a7ca75d5f
Takashi Sato <takashi@tks.st>
2022-07-30 19:39:31 +0900

    refactor: remove the meaningless override

--- typescript.js
+++ typescript.js
@@ -16,8 +16,6 @@ module.exports = {
         "@typescript-eslint/no-empty-function": "off",
         "@typescript-eslint/no-floating-promises": ["error", { ignoreIIFE: true }],
         "@typescript-eslint/no-use-before-define": "off",
-        "@typescript-eslint/no-explicit-any": "warn",
-        "@typescript-eslint/no-unused-vars": "warn",
         "prefer-const": "warn",
         // from https://github.com/iamturns/eslint-config-airbnb-typescript/blob/4aec5702be5b4e74e0e2f40bc78b4bc961681de1/lib/shared.js#L230-L240
         "import/extensions": ["error", "ignorePackages"],
