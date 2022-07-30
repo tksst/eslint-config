1446c9fa1f1d4b8b6c9feff90d435f719885806c
Takashi Sato <takashi@tks.st>
2022-07-30 19:34:29 +0900

    fix: add ignoreIIFE to @typescript-eslint/no-floating-promises
    Without this setting, async functions cannot be executed at the top level (Node.js 12 does not support Top Level Await)

--- typescript.js
+++ typescript.js
@@ -14,6 +14,7 @@ module.exports = {
     rules: {
         "@typescript-eslint/explicit-function-return-type": "off",
         "@typescript-eslint/no-empty-function": "off",
+        "@typescript-eslint/no-floating-promises": ["error", { ignoreIIFE: true }],
         "@typescript-eslint/no-use-before-define": "off",
         "@typescript-eslint/no-explicit-any": "warn",
         "@typescript-eslint/no-unused-vars": "warn",
