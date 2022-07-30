f464a02eaed8b41326604573e9568f39501a27d4
Takashi Sato <takashi@tks.st>
2022-07-30 12:41:59 +0900

    feat: introduce @typescript-eslint/recommended-requiring-type-checking
    BREAKING CHANGE: This can make a lot of errors in existing code.

--- typescript.js
+++ typescript.js
@@ -5,6 +5,7 @@ module.exports = {
         "./javascript.js",
         "./airbnb-typescript-base-override.js",
         "plugin:@typescript-eslint/recommended",
+        "plugin:@typescript-eslint/recommended-requiring-type-checking",
         "prettier",
     ],
     parserOptions: {
@@ -14,8 +15,12 @@ module.exports = {
         "@typescript-eslint/explicit-function-return-type": "off",
         "@typescript-eslint/no-empty-function": "off",
         "@typescript-eslint/no-use-before-define": "off",
-        "@typescript-eslint/no-explicit-any": "off",
+        "@typescript-eslint/no-explicit-any": "warn",
         "@typescript-eslint/no-unused-vars": "warn",
+        "@typescript-eslint/no-unsafe-assignment": "warn",
+        "@typescript-eslint/no-unsafe-member-access": "warn",
+        "@typescript-eslint/no-unsafe-return": "warn",
+        "@typescript-eslint/no-unsafe-argument": "warn",
         "prefer-const": "warn",
         // from https://github.com/iamturns/eslint-config-airbnb-typescript/blob/4aec5702be5b4e74e0e2f40bc78b4bc961681de1/lib/shared.js#L230-L240
         "import/extensions": ["error", "ignorePackages"],
