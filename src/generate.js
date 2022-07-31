417810b1d1b5a00112a1a7b983950fc07872b7b2
Takashi Sato <takashi@tks.st>
2022-07-31 09:54:34 +0900

    feat: trim the rules

--- src/generate.js
+++ src/generate.js
@@ -1,6 +1,16 @@
 const { ESLint } = require("eslint");
 const fs = require("fs/promises");
 
+function trimRules(conf) {
+    Object.entries(conf.rules)
+        // eslint-disable-next-line no-unused-vars
+        .filter(([key, value]) => Array.isArray(value) && (value[0] === "off" || value[0] === 0))
+        // eslint-disable-next-line no-unused-vars
+        .forEach(([key, value]) => {
+            conf.rules[key] = undefined;
+        });
+}
+
 (async () => {
     await fs.mkdir("./dist");
 })();
@@ -13,6 +23,7 @@ const fs = require("fs/promises");
     });
 
     const result = await eslint.calculateConfigForFile("./test.js");
+    trimRules(result);
     result.ignorePatterns = undefined;
     await fs.writeFile("./dist/javascript.json", JSON.stringify(result, undefined, 2));
 })();
@@ -25,6 +36,7 @@ const fs = require("fs/promises");
     });
 
     const result = await eslint.calculateConfigForFile("./test.ts");
+    trimRules(result);
     result.ignorePatterns = undefined;
     result.parser = "@typescript-eslint/parser";
     await fs.writeFile("./dist/typescript.json", JSON.stringify(result, undefined, 2));
