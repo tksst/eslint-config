05aebb304af0c341f59c18c131f62f50a901a31e
Takashi Sato <takashi@tks.st>
2022-08-03 11:59:33 +0900

    test: refactor test

--- src/javascript-json.test.js
+++ src/javascript-json.test.js
@@ -1,13 +1,12 @@
 const { ESLint } = require("eslint");
 const relativize = require("./relativize.js");
 
-const eslint = new ESLint({
-    useEslintrc: false,
-    overrideConfigFile: "./dist/javascript.json",
-});
-
 describe("ESLint config Snapshot", () => {
-    it("for JavaScript", async () => {
+    it("dist/javascript.json", async () => {
+        const eslint = new ESLint({
+            useEslintrc: false,
+            overrideConfigFile: "./dist/javascript.json",
+        });
         const result = await eslint.calculateConfigForFile("./test.js");
         relativize(result);
         expect(result).toMatchSnapshot();
--- src/typescript-json.test.js
+++ src/typescript-json.test.js
@@ -1,13 +1,12 @@
 const { ESLint } = require("eslint");
 const relativize = require("./relativize.js");
 
-const eslint = new ESLint({
-    useEslintrc: false,
-    overrideConfigFile: "./dist/typescript.json",
-});
-
 describe("ESLint config Snapshot", () => {
-    it("for TypeScript", async () => {
+    it("dist/typescript.json", async () => {
+        const eslint = new ESLint({
+            useEslintrc: false,
+            overrideConfigFile: "./dist/typescript.json",
+        });
         const result = await eslint.calculateConfigForFile("./test.ts");
         relativize(result);
         expect(result).toMatchSnapshot();
