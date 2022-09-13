bb15ec25cfd0679685786ff4dcd215b5541d14e1
Takashi Sato <takashi@tks.st>
2022-09-13 22:17:45 +0900

    feat: enable typescript-eslint-strict rules and also add some rules for better code.
    BREAKING CHANGES: This change may cause errors in the current code.

--- src/typescript-rule-base.js
+++ src/typescript-rule-base.js
@@ -6,6 +6,7 @@ module.exports = {
         "eslint-config-airbnb-typescript/base",
         "plugin:@typescript-eslint/recommended",
         "plugin:@typescript-eslint/recommended-requiring-type-checking",
+        "plugin:@typescript-eslint/strict",
         "prettier",
     ],
     parserOptions: {
@@ -16,6 +17,12 @@ module.exports = {
         "@typescript-eslint/no-empty-function": "off",
         "@typescript-eslint/no-floating-promises": ["error", { ignoreIIFE: true }],
         "@typescript-eslint/no-use-before-define": "off",
+        "@typescript-eslint/strict-boolean-expressions": "error",
+        "@typescript-eslint/prefer-readonly": "warn",
+        "@typescript-eslint/consistent-type-imports": "warn",
+        "@typescript-eslint/explicit-member-accessibility": "warn",
+        "@typescript-eslint/no-redundant-type-constituents": "warn",
+        "no-useless-empty-export": "warn",
         "prefer-const": "warn",
         // from https://github.com/iamturns/eslint-config-airbnb-typescript/blob/4aec5702be5b4e74e0e2f40bc78b4bc961681de1/lib/shared.js#L230-L240
         "import/extensions": ["error", "ignorePackages"],
