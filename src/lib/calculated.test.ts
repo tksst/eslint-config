29eecdafd269b94aeca9452393c5ac60fda3e6ad
Takashi Sato <takashi@tks.st>
2023-11-16 01:10:02 +0900

    test: remove parser version from snapshots

--- src/lib/calculated.test.ts
+++ src/lib/calculated.test.ts
@@ -1,16 +1,25 @@
 import { FlatESLint } from "eslint/use-at-your-own-risk";
 
+/* eslint-disable @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access */
+
 describe("ESLint calculated config Snapshot", () => {
-    /* eslint-disable @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access */
     it(".js", async () => {
         const eslint = new FlatESLint();
         const conf = await eslint.calculateConfigForFile("index.js");
-        expect(conf).toMatchSnapshot();
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/non-nullable-type-assertion-style
+        const conf2 = JSON.parse(JSON.stringify(conf) as string) as any;
+        conf2.languageOptions.parser = conf2.languageOptions.parser.replace(/@[^@]+$/, "");
+
+        expect(conf2).toMatchSnapshot();
     });
     it(".ts", async () => {
         const eslint = new FlatESLint();
         const conf = await eslint.calculateConfigForFile("index.ts");
-        expect(conf).toMatchSnapshot();
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/non-nullable-type-assertion-style
+        const conf2 = JSON.parse(JSON.stringify(conf) as string) as any;
+        conf2.languageOptions.parser = conf2.languageOptions.parser.replace(/@[^@]+$/, "");
+
+        expect(conf2).toMatchSnapshot();
     });
     /* eslint-enable */
 });
