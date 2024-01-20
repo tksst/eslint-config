276faebba27ff0f0b3c9384552ae2f6625c1b47e
Takashi Sato <takashi@tks.st>
2024-01-21 00:28:32 +0900

    test: improve the snapshot tests

--- src/lib/index.test.ts
+++ src/lib/index.test.ts
@@ -20,23 +20,26 @@ function removeParser(c: Linter.FlatConfig[]) {
     });
 }
 
-describe("ESLint config Snapshot", () => {
+describe("config snapshot", () => {
+    it("config.typeScript", () => {
+        expect(removeParser(removePlugin(config.config.typeScript))).toMatchSnapshot();
+    });
+    it("config.javaScript", () => {
+        expect(removeParser(removePlugin(config.config.javaScript))).toMatchSnapshot();
+    });
+    it("config.typeScriptOnly", () => {
+        expect(removeParser(removePlugin(config.config.typeScriptOnly))).toMatchSnapshot();
+    });
+    it("config.jest", () => {
+        expect(removeParser(removePlugin([config.config.jest]))).toMatchSnapshot();
+    });
+});
+
+describe("preset snapshots", () => {
     it("preset.typeScript", () => {
         expect(removeParser(removePlugin(config.preset.typeScript()))).toMatchSnapshot();
     });
     it("preset.typeScript jest: true", () => {
         expect(removeParser(removePlugin(config.preset.typeScript({ jest: true })))).toMatchSnapshot();
-    });
-    it("rules.typeScript", () => {
-        expect(removeParser(removePlugin(config.config.typeScript))).toMatchSnapshot();
-    });
-    it("rules.javaScript", () => {
-        expect(removeParser(removePlugin(config.config.javaScript))).toMatchSnapshot();
-    });
-    it("rules.typeScriptOnly", () => {
-        expect(removeParser(removePlugin(config.config.typeScriptOnly))).toMatchSnapshot();
-    });
-    it("rules.jest", () => {
-        expect(removeParser(removePlugin([config.config.jest]))).toMatchSnapshot();
     });
 });
