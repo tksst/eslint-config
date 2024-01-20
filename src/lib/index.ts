f4fa42a5059f2993c58d6bc89982f8d7ec2b695b
Takashi Sato <takashi@tks.st>
2024-01-20 22:27:12 +0900

    refactor: merge two rule files to index.ts

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -8,10 +8,8 @@ import globals from "globals";
 
 import airBnbBase from "./configs/airbnbBase.js";
 import airBnbTsBase from "./configs/airbnbTSBase.js";
-import javascriptRule from "./configs/javascript-rule-base.js";
 import jest from "./configs/jest.js";
 import regexp from "./configs/regexp.js";
-import typescriptRule from "./configs/typescript-rule-base.js";
 import typescriptEslint from "./configs/typescriptEslint.js";
 
 const javaScript = [
@@ -28,7 +26,99 @@ const javaScript = [
             // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
             unicorn: pluginUnicorn,
         },
-        rules: javascriptRule.rules,
+        rules: {
+            "no-console": "off",
+            "no-empty": ["error", { allowEmptyCatch: true }],
+            "no-empty-static-block": "warn",
+            "no-restricted-syntax": "off",
+            "no-plusplus": "off",
+            "no-continue": "off",
+            "no-bitwise": "off",
+            "no-param-reassign": "off",
+            "no-await-in-loop": "warn",
+            "no-empty-function": "off",
+            "no-unused-vars": "warn",
+            "default-case": "off",
+            "prefer-const": "warn",
+            "prefer-destructuring": [
+                "error",
+                {
+                    AssignmentExpression: {
+                        array: false,
+                        object: false,
+                    },
+                    VariableDeclarator: {
+                        array: false,
+                        object: true,
+                    },
+                },
+                {
+                    enforceForRenamedProperties: false,
+                },
+            ],
+            "prefer-regex-literals": "off",
+            "class-methods-use-this": "off",
+            "global-require": "off", // this is DEPRECATED in ESLint v7.0.0
+            "max-classes-per-file": "off",
+
+            "simple-import-sort/imports": "warn",
+            "simple-import-sort/exports": "warn",
+            "sort-imports": "off",
+            "redos/no-vulnerable": "error",
+
+            // Require escape sequences to use uppercase values.
+            "unicorn/escape-case": "error",
+            // Enforce the use of Unicode escapes instead of hexadecimal escapes.
+            "unicorn/no-hex-escape": "error",
+            // Require Array.isArray() instead of instanceof Array.
+            "unicorn/no-instanceof-array": "error",
+            // Prevent calling EventTarget#removeEventListener() with the result of an expression.
+            "unicorn/no-invalid-remove-event-listener": "error",
+            // Enforce the use of Buffer.from() and Buffer.alloc() instead of the deprecated new Buffer().
+            "unicorn/no-new-buffer": "error",
+            // Disallow classes that only have static members.
+            "unicorn/no-static-only-class": "error",
+            // Disallow comparing undefined using typeof.
+            "unicorn/no-typeof-undefined": "error",
+            // Disallow unnecessary spread.
+            "unicorn/no-useless-spread": "error",
+            // Enforce proper case for numeric literals.
+            "unicorn/number-literal-case": "error",
+            // Enforce the style of numeric separators by correctly grouping digits.
+            "unicorn/numeric-separators-style": ["error", { onlyIfContainsSeparator: true }],
+            // Prefer .find(…) and .findLast(…) over the first or last element from .filter(…)
+            "unicorn/prefer-array-find": "error",
+            // Prefer Array#{indexOf,lastIndexOf}() over Array#{findIndex,findLastIndex}() when looking for the index of an item.
+            "unicorn/prefer-array-index-of": "error",
+            // Prefer .some(…) over .filter(…).length check and .{find,findLast}(…).
+            "unicorn/prefer-array-some": "error",
+            // Prefer Blob#arrayBuffer() over FileReader#readAsArrayBuffer(…) and Blob#text() over FileReader#readAsText(…).
+            "unicorn/prefer-blob-reading-methods": "error",
+            // Prefer Date.now() to get the number of milliseconds since the Unix Epoch
+            "unicorn/prefer-date-now": "error",
+            // Prefer default parameters over reassignment
+            "unicorn/prefer-default-parameters": "error",
+            // Prefer .includes() over .indexOf() and Array#some() when checking for existence or non-existence
+            "unicorn/prefer-includes": "error",
+            // Prefer using a logical operator over a ternary
+            "unicorn/prefer-logical-operator-over-ternary": "error",
+            // Prefer using String, Number, BigInt, Boolean, and Symbol directly.
+            "unicorn/prefer-native-coercion-functions": "error",
+            // Prefer using the node: protocol when importing Node.js builtin modules.
+            "unicorn/prefer-node-protocol": "error",
+            // Prefer Number static properties over global ones.
+            "unicorn/prefer-number-properties": "error",
+            // Prefer omitting the catch binding parameter
+            "unicorn/prefer-optional-catch-binding": "error",
+            // Prefer RegExp#test() over String#match() and RegExp#exec()
+            "unicorn/prefer-regexp-test": "error",
+            // Prefer String#startsWith() & String#endsWith() over RegExp#test()
+            "unicorn/prefer-string-starts-ends-with": "error",
+            // Prefer String#trimStart() / String#trimEnd() over String#trimLeft() / String#trimRight()
+            "unicorn/prefer-string-trim-start-end": "error",
+            // Enforce throwing TypeError in type checking conditions
+            "unicorn/prefer-type-error": "error",
+        },
     },
     regexp,
 ] satisfies Linter.FlatConfig[];
@@ -38,7 +128,19 @@ const typeScriptOnly = [
     typescriptEslint,
     { rules: configPrettier.rules },
     {
-        rules: typescriptRule.rules,
+        rules: {
+            "@typescript-eslint/explicit-function-return-type": "off",
+            "@typescript-eslint/no-empty-function": "off",
+            "@typescript-eslint/no-floating-promises": ["error", { ignoreIIFE: true }],
+            "@typescript-eslint/no-use-before-define": "off",
+            "@typescript-eslint/strict-boolean-expressions": "error",
+            "@typescript-eslint/prefer-readonly": "warn",
+            "@typescript-eslint/consistent-type-imports": "warn",
+            "@typescript-eslint/explicit-member-accessibility": "warn",
+            "@typescript-eslint/no-redundant-type-constituents": "warn",
+            "@typescript-eslint/no-useless-empty-export": "warn",
+            "prefer-const": "warn",
+        },
     },
 ] satisfies Linter.FlatConfig[];
 
