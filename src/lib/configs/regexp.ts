27d0785afa7844fdb4c6e34c81614db0f58c8066
Takashi Sato <takashi@tks.st>
2024-03-13 18:43:05 +0900

    refactor: migrate to the flat config of eslint-plugin-regexp

--- src/lib/configs/regexp.ts
+++ src/lib/configs/regexp.ts
@@ -1,13 +1,7 @@
 import type { Linter } from "eslint";
 import * as pluginRegexp from "eslint-plugin-regexp";
 
-const x = {
-    plugins: {
-        // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-        regexp: pluginRegexp,
-    },
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access
-    rules: pluginRegexp.configs.recommended.rules,
-} satisfies Linter.FlatConfig;
+// eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+const x = pluginRegexp.configs["flat/recommended"] as Linter.FlatConfig;
 
 export default x;
