28a14b992184e3fd47d7cc154fa6e911409ea7c5
Takashi Sato <takashi@tks.st>
2024-04-12 22:11:35 +0900

    refactor: migrate from @typescript-eslint/plugin and  parser to typescript-eslint

--- src/lib/external-config/typescriptEslint.ts
+++ src/lib/external-config/typescriptEslint.ts
@@ -1,58 +1,33 @@
-import tsEslintPlugin from "@typescript-eslint/eslint-plugin";
-import tsEslintParser from "@typescript-eslint/parser";
 import type { ESLint, Linter } from "eslint";
+import { configs, parser, plugin } from "typescript-eslint";
 
 import { EXTERNAL } from "../name.js";
 
-const x1 = tsEslintPlugin.configs["eslint-recommended"]?.overrides?.[0]?.rules as Linter.RulesRecord | undefined;
+type FlatConfig = typeof configs.base;
 
-if (x1 === undefined) {
-    throw new TypeError("unexpected @typescript-eslint/plugin eslint-recommended config");
+function removeFilesEntry(config: FlatConfig) {
+    delete config.files;
+    return config;
 }
 
-const x2 = tsEslintPlugin.configs["strict-type-checked"]?.rules as Linter.RulesRecord | undefined;
-
-if (x2 === undefined) {
-    throw new TypeError("unexpected @typescript-eslint/plugin strict-type-checked config");
-}
-
-const x3 = tsEslintPlugin.configs["stylistic-type-checked"]?.rules as Linter.RulesRecord | undefined;
-
-if (x3 === undefined) {
-    throw new TypeError("unexpected @typescript-eslint/plugin stylistic-type-checked config");
-}
+const strictTypeCheckedRules = configs.strictTypeChecked.map(removeFilesEntry);
+const stylisticTypeCheckedRules = configs.stylisticTypeChecked.map(removeFilesEntry);
 
 const x = [
     {
         name: `${EXTERNAL}/@typescript-eslint/load-plugins-and-parser`,
         plugins: {
-            "@typescript-eslint": tsEslintPlugin as unknown as ESLint.Plugin,
+            "@typescript-eslint": plugin as unknown as ESLint.Plugin,
         },
         languageOptions: {
-            parser: tsEslintParser as unknown as Linter.ParserModule,
+            parser: parser as unknown as Linter.FlatConfigParserModule,
             parserOptions: {
                 project: "./tsconfig.json",
             },
         },
     },
-    {
-        name: `${EXTERNAL}/@typescript-eslint/eslint-recommended`,
-        rules: {
-            ...x1,
-        },
-    },
-    {
-        name: `${EXTERNAL}/@typescript-eslint/strict-type-checked`,
-        rules: {
-            ...x2,
-        },
-    },
-    {
-        name: `${EXTERNAL}/@typescript-eslint/stylistic-type-checked`,
-        rules: {
-            ...x3,
-        },
-    },
+    ...(strictTypeCheckedRules as Linter.FlatConfig[]),
+    ...(stylisticTypeCheckedRules as Linter.FlatConfig[]),
 ] satisfies Linter.FlatConfig[];
 
 export default x;
